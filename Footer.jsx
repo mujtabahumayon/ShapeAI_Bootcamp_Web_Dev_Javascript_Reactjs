81ebe333b8eac9e3520297e2bf6e09688239232e
mujtabahumayon <37635862+mujtabahumayon@users.noreply.github.com>
2021-06-26 16:36:52 +0500

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        <p>Copyright &copy; by ShapeAI @ {new Date().getFullYear()}</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript & React.js</h1>
+      <p>A basic web development React Js Bootcamp</p>
+    </div>
+  );
+}
+
+export default Info;
